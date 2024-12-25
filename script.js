f3cc3046534e0b25c4dd660f99391aed96930060
Aeroshide <huskyisbadboi61@gmail.com>
2024-12-26 01:28:16 +0700

    rawr

--- script.js
+++ script.js
@@ -11,7 +11,9 @@ let zenFeedback = '';
 function getCookie(name) {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
+    if (parts.length === 2) {
+        return parts.pop().split(';').shift().replace(/%20/g, ' ');
+    }
     return null;
 }
 
